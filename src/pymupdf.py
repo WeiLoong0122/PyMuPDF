fcb890eb742292d3078360f1e416cd8e95dbafaf
WeiLoong0122 <113124335+WeiLoong0122@users.noreply.github.com>
2024-12-11 17:18:10 +0800

    test pymupdf.py

--- src/pymupdf.py
+++ src/pymupdf.py
@@ -1,2 +1,7 @@
 # pylint: disable=wildcard-import,unused-import
-from . import *
+import fitz  # PyMuPDF
+
+def extract_vector_and_text_from_pdf(~/Downloads/2024-12-10 TOPAZ PILING  LAYOUT (Phase 1 & 2)_WITH SCHEDULE_120%.pdf):
+    doc = fitz.open(~/Downloads/2024-12-10 TOPAZ PILING  LAYOUT (Phase 1 & 2)_WITH SCHEDULE_120%.pdf)
+    text_elements = []
+    vector_elements = []
